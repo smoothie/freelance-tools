861775c862491819fd04ab56fa608a0a413a0d36
Marc Eichenseher <this@marceichenseher.de>
2024-07-15 18:05:49 +0200

    Add Symfony framework bundle and stuff

---
+++ src/Contracts/ControllerContract.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Smoothie\ContractorTools\Contracts;
+
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
+
+#[AutoconfigureTag('controller.service_arguments')]
+interface ControllerContract
+{
+}
