79903a1287401621f7cc36b8ec39cd375f77a6e1
Marc Eichenseher <this@marceichenseher.de>
2024-07-18 14:45:13 +0200

    Setup doctrine and maker bundle
    And I tried to disable the ORM as I simply want to use a DBAL.. let's
    see how far we get with that.

--- config/bundles.php
+++ config/bundles.php
@@ -3,5 +3,9 @@
 declare(strict_types=1);
 
 return [
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
