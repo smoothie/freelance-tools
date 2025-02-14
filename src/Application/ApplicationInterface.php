8cc2c64c28919589b55a67f4551fc77ac2a76586
Marc Eichenseher <this@marceichenseher.de>
2025-02-14 05:00:44 +0100

    Restart, again

--- src/Application/ApplicationInterface.php
+++ src/Application/ApplicationInterface.php
@@ -4,20 +4,9 @@ declare(strict_types=1);
 
 namespace App\Application;
 
-use App\Domain\Models\Task\TaskId;
-use App\Domain\Models\WorkTime\WorkTimeProcessor;
-use App\Domain\Models\WorkTime\WorkTimeProvider;
-use App\Domain\Models\WorkTime\WorkTimeProviderId;
-
 interface ApplicationInterface
 {
-    public function importATask(ImportTask $command): void;
-
-    public function trackTime(TaskId $taskId, TrackTiming $command): void;
-
-    public function setupWorkTimeProvider(SetupWorkTimeProvider $command): void;
-
-    public function downloadTasks(DownloadTasks $command): void;
-
+        public function generateTimesheet(GenerateTimesheet $command): void;
+    //    public function importTasks(WorkTimeProvider $workTimeProvider): void;
     //    public function render(Component $component): void;
 }
--- tests/Acceptance/ServiceContainerForAcceptanceTesting.php
+++ tests/Acceptance/ServiceContainerForAcceptanceTesting.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Tests\Acceptance;
 
-use App\Application\EventDispatcherWithSubscribers;
 use App\Infrastructure\ServiceContainer;
 use Test\Acceptance\EventDispatcherSpy;
 
@@ -12,34 +11,34 @@ final class ServiceContainerForAcceptanceTesting extends ServiceContainer
 {
     private ?EventDispatcherSpy $eventDispatcherSpy = null;
 
-    protected function registerEventSubscribers(EventDispatcherWithSubscribers $eventDispatcher): void
-    {
-        parent::registerEventSubscribers($eventDispatcher);
-
-        $eventDispatcher->subscribeToAllEvents([$this->eventDispatcherSpy(), 'notify']);
-
-        // Test-specific listeners:
-        //        $eventDispatcher->subscribeToSpecificEvent(
-        //            SessionWasPlanned::class,
-        //            [$this->sessions(), 'whenSessionWasPlanned']
-        //        );
-    }
-
-    //    protected function purchaseRepository(): PurchaseRepository
-    //    {
-    //        if ($this->purchaseRepository === null) {
-    //            $this->purchaseRepository = new PurchaseRepositoryInMemory();
-    //        }
-    //
-    //        return $this->purchaseRepository;
-    //    }
-
-    public function eventDispatcherSpy(): EventDispatcherSpy
-    {
-        if ($this->eventDispatcherSpy === null) {
-            $this->eventDispatcherSpy = new EventDispatcherSpy();
-        }
-
-        return $this->eventDispatcherSpy;
-    }
+//    protected function registerEventSubscribers(EventDispatcherWithSubscribers $eventDispatcher): void
+//    {
+//        parent::registerEventSubscribers($eventDispatcher);
+//
+//        $eventDispatcher->subscribeToAllEvents([$this->eventDispatcherSpy(), 'notify']);
+//
+//        // Test-specific listeners:
+//        //        $eventDispatcher->subscribeToSpecificEvent(
+//        //            SessionWasPlanned::class,
+//        //            [$this->sessions(), 'whenSessionWasPlanned']
+//        //        );
+//    }
+//
+//    //    protected function purchaseRepository(): PurchaseRepository
+//    //    {
+//    //        if ($this->purchaseRepository === null) {
+//    //            $this->purchaseRepository = new PurchaseRepositoryInMemory();
+//    //        }
+//    //
+//    //        return $this->purchaseRepository;
+//    //    }
+//
+//    public function eventDispatcherSpy(): EventDispatcherSpy
+//    {
+//        if ($this->eventDispatcherSpy === null) {
+//            $this->eventDispatcherSpy = new EventDispatcherSpy();
+//        }
+//
+//        return $this->eventDispatcherSpy;
+//    }
 }
